b4b79c3abf5aea7546469cc9684f4b0c657530fc
Peter Holdsworth <peter.holdsworth@and.digital>
2023-11-28 13:11:19 +0000

    dataset now returns label_matrix

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ from matplotlib.patches import Rectangle
 class DigitsDataset(torch.utils.data.Dataset):
   def __init__(self):
     self.trn_ds = torchvision.datasets.MNIST(root="data", train=True,  download=True, transform=torchvision.transforms.ToTensor())
-
+  
   def __getitem__(self, idx):
     image, digit_label = self.trn_ds.__getitem__(idx)
     new_size = random.randint(c.MINIMUM_DIGIT_SIZE, min(c.IMAGE_HEIGHT, c.IMAGE_WIDTH) - 2 * c.MARGIN)
@@ -18,23 +18,33 @@ class DigitsDataset(torch.utils.data.Dataset):
     resized_image = TF.resize(image, new_size)
     return_image = torch.zeros(c.IMAGE_HEIGHT, c.IMAGE_WIDTH)
     return_image[y1:y1+new_size, x1:x1+new_size] = resized_image
-    return_image = return_image > c.WHITE_THRESHOLD
+    return_image = return_image > c.MASK_THRESHOLD
     xmin, ymin, xmax, ymax = torchvision.ops.masks_to_boxes(return_image.unsqueeze(0))[0]
     x, y, w, h = (xmin+xmax)/2, (ymin+ymax)/2, xmax-xmin, ymax-ymin
-    return return_image, digit_label, x, y, w, h
+    label_matrix = self.calculate_label_matrix(digit_label, x, y, w, h)
+    return return_image, label_matrix, digit_label, x, y, w, h
 
   def __len__(self):
     return self.trn_ds.__len__()
 
+  def calculate_label_matrix(self, digit_label, x, y, w, h):
+    label_matrix = torch.zeros((c.SR, c.SC, c.C + 5 * c.B))
+    x, y = x / c.IMAGE_WIDTH, y / c.IMAGE_HEIGHT
+    i, j = int(c.SR * y), int(c.SC * x)
+    x_cell, y_cell = c.SC * x - j, c.SR * y - i
+    w_cell, h_cell = c.SC * w / c.IMAGE_WIDTH, c.SR * h / c.IMAGE_HEIGHT
+    label_matrix[i, j, digit_label] = 1 # one hot encoding
+    label_matrix[i, j, c.C] = 1 # cell contains centre of digit
+    label_matrix[i, j, c.C+1:c.C+5] = torch.tensor([ x_cell, y_cell, w_cell, h_cell ])
+    return label_matrix
+
 if __name__ == "__main__":
   dd = DigitsDataset()
   while True:
-    image, digit_label, x, y, w, h = dd.__getitem__(random.randint(0,dd.__len__()))
-
+    image, label_matrix, digit_label, x, y, w, h = dd.__getitem__(random.randint(0,dd.__len__()))
     fig, ax = plt.subplots()
     ax.set_title(digit_label)
     ax.imshow(image.squeeze(0).numpy(), cmap='gray')
     ax.add_patch(Rectangle((x-w/2,y-h/2),width=w, height=h, edgecolor='Red', facecolor='none'))
-    
     plt.axis('off')
     plt.show()
